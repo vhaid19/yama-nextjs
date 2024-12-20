0b5ce55e534af1abbb1388bd620e141013824cbb
Vahid <vahid.yama@gmail.com>
2024-12-20 18:44:54 +0530

    start the history webpage from scrach

--- src/app/about/history/page.js
+++ src/app/about/history/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function History() {
   return (
-    <div>History</div>
+    <div>
+      <div className='Typing-effect'>
+        
+        </div>
+    </div>
   )
 }
 
