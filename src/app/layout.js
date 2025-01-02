ca4ea42f18f4e854ca409e775b9cff328d909bbc
Vahid <vahid.yama@gmail.com>
2025-01-02 16:52:02 +0530

    remove font into layout pagte

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -22,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
+        className={`antialiased bg-black text-white`}
       >
         <Navbar />
         {children}
